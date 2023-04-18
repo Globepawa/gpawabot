f1788546bd4e3f2697a5aae4fdda277bbf6c447c
Globepawa <130757700+Globepawa@users.noreply.github.com>
2023-04-18 14:00:53 +0300

    commit

---
+++ cool.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){ 
+$('.top').show(4000).append('<h2>G pawa</h2>').css({'color':'black', 'font-size':'larger'
+});
+$('.top').append('<h3>Your Financial partner</h3>').show(3000)
+
+ });
